7b58db1efac02cd2d8d9064e084b2653fd9fdd26
Bryce Basista <basista.bryce@gmail.com>
2018-10-03 20:26:30 -0400

    Questions 1 through 3 complete

--- SQL_HW/SQL_Sakila_HW.sql
+++ SQL_HW/SQL_Sakila_HW.sql
@@ -1,9 +1,17 @@
 Use sakila;
 -- 1a. Display the first and last names of all actors from the table `actor`.
+SELECT first_name, last_name
+FROM sakila.actor;
 
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column `Actor Name`.
+SELECT concat(first_name,' ', last_name)
+AS Actor_Name
+FROM sakila.actor
+ORDER BY last_name;
 
 -- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
+
+
 
 -- 2b. Find all actors whose last name contain the letters `GEN`:
 
